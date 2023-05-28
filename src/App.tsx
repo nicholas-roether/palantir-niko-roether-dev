de5be699252f54b2901ec44fa050797a323b7eb9
Nicholas Roether <nicholas.roether@t-online.de>
2023-05-28 17:59:17 +0200

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { JSX } from "solid-js";
+import { Heading } from "@nicholas-roether/palantir-ui-solid";
+
+function App(): JSX.Element {
+	return <Heading size="1">Hello World!</Heading>;
+}
+
+export default App;
