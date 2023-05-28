4a438a7170e4ac238b194a117004ecdf020417ce
Nicholas Roether <nicholas.roether@t-online.de>
2023-05-28 18:46:08 +0200

    Implement join string decoding

---
+++ src/join_string.ts
@@ -0,0 +1,26 @@
+interface JoinInfo {
+	hostName: string;
+	hostId: string;
+}
+
+const JOIN_STRING_REGEX = /^([^:]+):([A-Za-z0-9+/=]+)$/;
+
+function parseClearJoinString(str: string): JoinInfo | null {
+	const result = JOIN_STRING_REGEX.exec(str);
+	if (!result) return null;
+	return {
+		hostName: result[1],
+		hostId: result[2]
+	};
+}
+
+function parseJoinString(str: string): JoinInfo | null {
+	try {
+		const clearString = atob(str);
+		return parseClearJoinString(clearString);
+	} catch (e) {
+		return null;
+	}
+}
+
+export { parseJoinString, JoinInfo };
--- src/routes/Join.tsx
+++ src/routes/Join.tsx
@@ -1,7 +1,38 @@
-import { JSX } from "solid-js";
+import { JSX, Show } from "solid-js";
 import { useParams } from "@solidjs/router";
 import { css } from "@emotion/css";
 import { Card, Heading, Paragraph } from "@nicholas-roether/palantir-ui-solid";
+import { JoinInfo, parseJoinString } from "../join_string";
+
+interface InvitationProps {
+	joinInfo: JoinInfo;
+}
+
+function Invitation({ joinInfo }: InvitationProps): JSX.Element {
+	return (
+		<>
+			<Heading size="1">An Invitation</Heading>
+			<Paragraph>
+				You can hear the eerie whispers of <b>{joinInfo.hostName}</b>{" "}
+				coming from beyond the shroud.
+			</Paragraph>
+		</>
+	);
+}
+
+function InvalidJoinString(): JSX.Element {
+	return (
+		<>
+			<Heading size="1">Nothing.</Heading>
+			<Paragraph>
+				The abyss peers back at you, black and empty. It does not understand your intentions.
+			</Paragraph>
+			<Paragraph>
+				<em>(Your join link is invalid.)</em>
+			</Paragraph>
+		</>
+	)
+}
 
 const pageWrapper = css`
 	width: 100vw;
@@ -14,15 +45,17 @@ const pageWrapper = css`
 
 function Join(): JSX.Element {
 	const params = useParams();
+	const joinInfo = parseJoinString(params.joinString);
 
 	return (
 		<div class={pageWrapper}>
 			<Card>
-				<Heading size="1">An Invitation</Heading>
-				<Paragraph>
-					You can hear the eerie whispers of{" "}
-					<b>{params.joinString}</b> coming from beyond the shroud.
-				</Paragraph>
+				<Show when={joinInfo != null}>
+					<Invitation joinInfo={joinInfo!} />
+				</Show>
+				<Show when={joinInfo == null}>
+					<InvalidJoinString />
+				</Show>
 			</Card>
 		</div>
 	);
