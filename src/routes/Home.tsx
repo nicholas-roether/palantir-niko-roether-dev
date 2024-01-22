059ff660b0e86433838c31584d49f239c4467dea
Nicholas Roether <nicholas.roether@t-online.de>
2024-01-22 21:11:49 +0100

    Update homepage

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,19 @@
 import { JSX } from "solid-js";
 import { Heading, Paragraph } from "@nicholas-roether/palantir-ui-solid";
+import { css } from "@emotion/css";
+
+const center = css`
+	text-align: center;
+`;
 
 function Home(): JSX.Element {
 	return (
 		<>
-			<Heading size="1">Turn back, ye lost soul</Heading>
-			<Paragraph>
-				This page is currently work-in-progress. Come back later!
+			<Heading size="1">Palantir</Heading>
+			<Paragraph class={center}>
+				If you've landed on this page, you've probably tried to join a Palantir
+				session without having the extension installed. That's not surprising,
+				since it's unfinished at the moment. Sucks to be you!
 			</Paragraph>
 		</>
 	);
